67d458ca410362bebc4cfeea44def68c435e70c0
rigonhodza <rigon.hoxha10@gmail.com>
2026-06-09 17:48:53 +0200

    ewrgf

---
+++ module_1/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <title> Intro to PHP</title> 
+    </head>
+    <body>
+           <h1>My First PHP Page</h1>
+           <?php echo "Hello World!";  ?>
+</body>
+</html>
